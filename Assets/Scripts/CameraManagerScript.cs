bb314d7a043a94e22c3f8e31963794c8c3c2150c
Jeroen Brouns <jeroenbrouns@wlan-161101.nbw.tue.nl>
2018-09-24 11:47:53 +0200

    Fixed rotation with array

--- Assets/Scripts/CameraManagerScript.cs
+++ Assets/Scripts/CameraManagerScript.cs
@@ -9,6 +9,10 @@ public class CameraManagerScript : MonoBehaviour {
     public Camera VRCamera;
     public Camera VRSimulatorCamera;
 
+    List<Vector3> prevRotations = new List<Vector3>();
+    int cameraLag = 0;
+    Vector3 rotateValue;
+
     // Use this for initialization
     void Start () {
         CameraOverview.enabled = true;
@@ -19,96 +23,30 @@ public class CameraManagerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         MoveCamera();
-        //StartCoroutine(ExecuteAfterTime(0.5f, () =>
-        //{
-        //    MoveCamera();
-        //}));
     }
 
-    private bool isCoroutineExecuting = false;
-
-    IEnumerator ExecuteAfterTime(float time, Action task)
-    {
-        if (isCoroutineExecuting)
-            yield break;
-        isCoroutineExecuting = true;
-        yield return new WaitForSeconds(time);
-        task();
-        isCoroutineExecuting = false;
-    }
-
-    private Vector3 currentAngle;
-    private Vector3 offset;
-    //public float delay = 2f; //For example here it waits 2 seconds.
-    public float followNow;
-
-
-    //void Start()
-    //{
-    //    followNow = Time.time + delay;
-    //    offset = transform.position;
-    //}
-
-    //void LateUpdate()
-    //{
-    //    if (Time.time > followNow)
-    //    {
-    //        if (player != null)
-    //        {
-    //            transform.position = player.transform.position + offset;
-    //        }
-    //        followNow += delay;
-    //    }
-    //}
-
-    Vector3 rotateValue;
-
-    internal void MoveCamera()
+    private void MoveCamera()
     {
         int speed = 70;
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            followNow = Time.time + 0.5f;
-            Debug.Log(followNow);
             rotateValue = rotateValue + new Vector3(0, speed * Time.deltaTime, 0);
-            Debug.Log(rotateValue);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            followNow = Time.time + 0.5f;
-            Debug.Log(followNow);
             rotateValue = rotateValue - new Vector3(0, speed * Time.deltaTime, 0);
-            Debug.Log(rotateValue);
         }
 
-        Debug.Log("follow_now timestamp: "+followNow+"   current time stamp: "+Time.time);
-
-        if (Input.GetKey(KeyCode.DownArrow))
+        prevRotations.Add(rotateValue);
+        if (prevRotations.Count > cameraLag)
         {
-            VRSimulatorCamera.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            Vector3 value = prevRotations[0];
+            prevRotations.RemoveAt(0);
+
+            VRSimulatorCamera.transform.eulerAngles = value;
         }
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            VRSimulatorCamera.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
-        }
-
-        if (Time.time > followNow)
-        {
-            Debug.Log("I expired");
-            currentAngle = VRSimulatorCamera.transform.eulerAngles;
-
-            currentAngle = new Vector3(
-                Mathf.LerpAngle(currentAngle.x, rotateValue.x, Time.deltaTime),
-                Mathf.LerpAngle(currentAngle.y, rotateValue.y, Time.deltaTime),
-                Mathf.LerpAngle(currentAngle.z, rotateValue.z, Time.deltaTime)
-            );
-
-            VRSimulatorCamera.transform.eulerAngles = currentAngle;
-        }
-
     }
 
     internal void ToggleCammera()
